7f5a3c043b643ea77e6bfdadc0f521e51ea96902
lotuni <hikaru.unity.1110@gmail.com>
2024-06-02 20:23:11 +0900

    hoge

--- js/edit-phone.js
+++ js/edit-phone.js
@@ -1,51 +1,46 @@
-var selFile = document.getElementById('file_choice'); // input type="file"の要素取得
+var selFile = document.getElementById('file_choice');
 var cleaeButton = document.getElementById('button');
-var c = document.getElementById('board'); // canvasの要素取得
+var c = document.getElementById('board');
 var ctx = c.getContext('2d');
 const tagImage = new Image();
 tagImage.src = "img/tag.png";
 
-let pos = {x: 0, y: 0};   // クリックしたXY座標
+let pos = {x: 0, y: 0};
 let rectPas = {x: 0, y: 0};
 
-// スタンプのサイズを指定
 const stampWidth = 30;
 const stampHeight = 30;
 
-// スタンプを描画する関数
 function drawStamp(x, y) {
   ctx.drawImage(tagImage, x-18, y+15, stampWidth, stampHeight);
 }
 
 c.addEventListener('touchstart', (event) => {
-  const rect = c.getBoundingClientRect();
-  const touch = event.touches[0];
-  const touchX = touch.clientX - rect.left;
-  const touchY = touch.clientY - rect.top;
-  drawStamp(touchX - stampWidth / 2, touchY - stampHeight / 2);
+  const rect = event.target.getBoundingClientRect();
+  pos.x = event.touches[0].clientX - rect.left;
+  pos.y = event.touches[0].clientY - rect.top;
+  drawStamp(pos.x - stampWidth / 2, pos.y - stampHeight / 2);
 });
 
 function clear(){
   ctx.clearRect(0, 0, c.width, c.height);
 }
 
-function touchStartListener(e){
+function touchDownListener(e){
     const rect = e.target.getBoundingClientRect();
-    const touch = e.touches[0];
-    pos.x = touch.clientX - rect.left;
-    pos.y = touch.clientY - rect.top;
+    pos.x = e.touches[0].clientX - rect.left;
+    pos.y = e.touches[0].clientY - rect.top;
 }
 
 selFile.addEventListener("change", function(evt){
- var file = evt.target.files; // fileの取得
+ var file = evt.target.files;
  var reader = new FileReader();
 
- reader.readAsDataURL(file[0]); // fileの要素をdataURL形式で読み込む
+ reader.readAsDataURL(file[0]);
 
- // ファイルを読み込んだ時に実行する
  reader.onload = function(){
-  var dataUrl = reader.result; // 読み込んだファイルURL
-  var img = new Image(); // 画像
+  var dataUrl = reader.result;
+  var img = new Image();
 
   img.src = dataUrl;
   img.onload=function(){
@@ -56,24 +51,17 @@ selFile.addEventListener("change", function(evt){
 }, false);
 
 window.addEventListener("load", function(){
-  // キャンバス情報取得
+  c.addEventListener("touchstart", touchDownListener, false);
 
-  // キャンバス上でタッチした時の処理
-  c.addEventListener("touchstart", touchStartListener, false);
-
-  c.addEventListener('touchstart', (event) => {
+  c.addEventListener('touchstart', () => {
     const rect = c.getBoundingClientRect();
-    const touch = event.touches[0];
-    rectPas.x = touch.clientX - rect.left;
-    rectPas.y = touch.clientY - rect.top;
+    rectPas.x = event.touches[0].clientX - rect.left;
+    rectPas.y = event.touches[0].clientY - rect.top;
   }, {once: false});
 
-  c.addEventListener('touchend', (event) => {
+  c.addEventListener('touchend', () => {
     const rect = c.getBoundingClientRect();
-    const touch = event.touches[0];
-    const endX = touch.clientX - rect.left;
-    const endY = touch.clientY - rect.top;
-    ctx.strokeRect(rectPas.x, rectPas.y, endX - rectPas.x, endY - rectPas.y);
+    ctx.strokeRect(rectPas.x, rectPas.y, pos.x - rectPas.x, pos.y - rectPas.y);
   }, {once: false});
 });
 
