22d7ccd7d9b2748d6065f523d39fe704efbcedd2
lotuni <hikaru.unity.1110@gmail.com>
2024-06-02 19:59:56 +0900

    hoge

--- js/edit-phone.js
+++ js/edit-phone.js
@@ -16,23 +16,19 @@ const stampHeight = 30;
 function drawStamp(x, y) {
   ctx.drawImage(tagImage, x-18, y+15, stampWidth, stampHeight);
 }
-
-c.addEventListener('touchstart', (event) => {
-  const touch = event.touches[0];
-  const touchX = touch.clientX - c.getBoundingClientRect().left;
-  const touchY = touch.clientY - c.getBoundingClientRect().top;
-  drawStamp(touchX - stampWidth / 2, touchY - stampHeight / 2);
+c.addEventListener('click', (event) => {
+  const mouseX = event.clientX - c.getBoundingClientRect().left;
+  const mouseY = event.clientY - c.getBoundingClientRect().top;
+  drawStamp(mouseX - stampWidth / 2, mouseY - stampHeight / 2);
 });
-
 function clear(){
   ctx.clearRect(0, 0, c.width, c.height);
 }
-
-function touchDownListener(e){
-    const touch = e.touches[0];
+function mouseDownListener(e){
     const rect = e.target.getBoundingClientRect();
-    pos.x = touch.clientX - rect.left;
-    pos.y = touch.clientY - rect.top;
+    pos.x = e.clientX - rect.left;
+    pos.y = e.clientY - rect.top;
+ 
 }
 
 selFile.addEventListener("change", function(evt){
@@ -55,22 +51,32 @@ selFile.addEventListener("change", function(evt){
 }, false);
 
 window.addEventListener("load", function(){
-  // キャンバス情報取得
+  // キャンバス情報取
 
-  // キャンバス上でタッチした時の処理
-  c.addEventListener("touchstart", touchDownListener, false);
+  // キャンバス上でマウスボタンを押した時の処理
+  c.addEventListener("mousedown", mouseDownListener, false);
+  c.addEventListener("mouseup", mouseDownListener, false);
+
+  c.addEventListener('mousedown', () => {
+    rectPas.x=pos.x;
+    rectPas.y=pos.y;
+  }, {once: false});
+  c.addEventListener('mouseup', () => {
+    ctx.strokeRect(rectPas.x,rectPas.y,pos.x-rectPas.x,pos.y-rectPas.y);
+  }, {once: false});
+
+  // 
+  c.addEventListener("touchstart", mouseDownListener, false);
+  c.addEventListener("touchstart", mouseDownListener, false);
 
   c.addEventListener('touchstart', () => {
     rectPas.x=pos.x;
     rectPas.y=pos.y;
   }, {once: false});
   c.addEventListener('touchend', () => {
-    const touch = event.changedTouches[0];
-    const rect = event.target.getBoundingClientRect();
-    const endX = touch.clientX - rect.left;
-    const endY = touch.clientY - rect.top;
-    ctx.strokeRect(rectPas.x, rectPas.y, endX - rectPas.x, endY - rectPas.y);
+    ctx.strokeRect(rectPas.x,rectPas.y,pos.x-rectPas.x,pos.y-rectPas.y);
   }, {once: false});
 });
 
-cleaeButton.addEventListener('touchstart', clear, false);
+cleaeButton.addEventListener('mousedown', clear, false);
+// c.addEventListener('mouseup', onMouseUp, false);
